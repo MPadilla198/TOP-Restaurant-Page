d35ba9217773e1df9a8e5080762498d66f0b6ee8
Miguel Padilla <mpadi21@mit.edu>
2025-11-24 15:34:44 -0600

    Correct export to prevent not found errors

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-export default Home = (() => {
+export default (() => {
     function  render(container) {
 
     };
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,4 @@
-export default Menu = (() => {
+export default (() => {
     function  render(container) {
 
     };
